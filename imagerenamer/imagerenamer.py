4b309c00cc38bdaa45caf5f0671f8bb7e6a0c25a
Alex Kenan <alexkenan@users.noreply.github.com>
2017-12-13 18:28:05 -0500

    Add files via upload
    Rewritten to run better when run from Terminal

--- imagerenamer/imagerenamer.py
+++ imagerenamer/imagerenamer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3.5
 #####################################
-#    LAST UPDATED     05 NOV 2017   #
+#    LAST UPDATED     13 DEC 2017   #
 #####################################
 """
 Renames photos to Year-Month-Day Hour.Minute.Second format
@@ -12,72 +12,79 @@ from PIL import Image
 from PIL.ExifTags import TAGS
 
 
-def main_png():
+def main_png(file: str):
     """
     Used for PNG images with EXIF data
+    :param file: Path to a photo to be renamed
     :return: None
     """
-    os.chdir('/Volumes/MAC2/Photos/')
-    dirs = os.listdir(os.getcwd())
-    for file in dirs:
-        if 'IMG_' in file and file.endswith('.png'):
-            img = Image.open(file)
-            print('Converting {}'.format(file))
-            creation_date = re.compile(
-                r'<photoshop:DateCreated>(\d)+-(\d)+-(\d)+T(\d)+:(\d)+:(\d)+</photoshop:DateCreated>')
-            before_time = 'blah'
-            for tag, value in img.info.items():
-                match = creation_date.search(str(value))
-                if match:
-                    before_time = match.group()
+    img = Image.open(file)
+    print('Converting {}'.format(file))
+    creation_date = re.compile(
+        r'<photoshop:DateCreated>(\d)+-(\d)+-(\d)+T(\d)+:(\d)+:(\d)+</photoshop:DateCreated>')
+    before_time = 'blah'
+    for tag, value in img.info.items():
+        match = creation_date.search(str(value))
+        if match:
+            before_time = match.group()
 
-            no_tags = before_time.replace('<photoshop:DateCreated>', '').replace('</photoshop:DateCreated>', '')
-            name = no_tags.replace(':', '.').replace('T', ' ')
-            name += '.png'
-            img.save(name)
-            print('Saved {}'.format(name))
-        else:
-            print('{} ends with JPG!'.format(file))
+    no_tags = before_time.replace('<photoshop:DateCreated>', '').replace('</photoshop:DateCreated>', '')
+    name = no_tags.replace(':', '.').replace('T', ' ')
+    name += '.png'
+    img.save(name)
+    print('Saved {}'.format(name))
 
 
-def main_jpg():
+def main_jpg(file: str):
     """
     Used for JPG or JPEG images with Photo Month Day, Hour:Minute:Second format
     If it can't determine EXIF date data, it uses the file name to fill
     in the rest of the name and leaves a "Y" for the user to enter the date
+    :param file: Path to a photo to be renamed
     :return: None
     """
-    print('-'*10, 'Starting', '-'*10)
-    os.chdir('/Volumes/MAC2/Photos/')
-    dirs = os.listdir(os.getcwd())
-    for file in dirs:
-        if file.startswith('Photo ') and not file.endswith('.png'):
-            print('Converting {}'.format(file))
-            image = Image.open(file)
-            info = image._getexif()
-            time = 'blah'
-            for tag, value in info.items():
-                key = TAGS.get(tag, tag)
-                if key == 'DateTime':
-                    time = str(value)
-                elif 'DateTime' in key:
-                    time = str(value)
-            try:
-                time_dt = datetime.datetime.strptime(time, '%Y:%m:%d %H:%M:%S')
-                name = '{0:%Y-%m-%d %H.%M.%S}.jpg'.format(time_dt)
-            except ValueError:
-                print('Could not rename {} with the year'.format(file))
-                filename = file.replace('Photo ', '').replace('.jpg', '')
-                time_dt = datetime.datetime.strptime(filename, '%b %d, %H %M %S %p')
-                name = 'Y-{0:%m-%d %H.%M.%S}.jpg'.format(time_dt)
+    print('Converting {}'.format(file))
+    image = Image.open(file)
+    info = image._getexif()
+    time = 'blah'
+    for tag, value in info.items():
+        key = TAGS.get(tag, tag)
+        if key == 'DateTime':
+            time = str(value)
+        elif 'DateTime' in key:
+            time = str(value)
+    try:
+        time_dt = datetime.datetime.strptime(time, '%Y:%m:%d %H:%M:%S')
+        name = '{0:%Y-%m-%d %H.%M.%S}.jpg'.format(time_dt)
+    except ValueError:
+        print('Could not rename {} with the year'.format(file))
+        filename = file.replace('Photo ', '').replace('.jpg', '')
+        time_dt = datetime.datetime.strptime(filename, '%b %d, %H %M %S %p')
+        name = 'Y-{0:%m-%d %H.%M.%S}.jpg'.format(time_dt)
 
-            image.save(name)
-            print('Saved {}'.format(name))
-        else:
-            print('{} ends with PNG, skipping'.format(file))
-
-    print('-'*10, 'Finished', '-'*10)
+    image.save(name)
+    print('Saved {}'.format(name))
 
 
 if __name__ == '__main__':
-    main_jpg()
+    print('-' * 10, 'Starting', '-' * 10)
+    yesno = input('Are photos to be renamed in /Volumes/MAC2/Photos/? (y/n) ')
+    yesno = yesno.lower()
+    if yesno == 'y':
+        os.chdir('/Volumes/MAC2/Photos/')
+    else:
+        newpath = input('Type in the full path to the photos: ')
+        if os.path.exists(newpath):
+            os.chdir(path)
+        else:
+            while not os.path.exists(newpath):
+                newpath = input("Try again! ")
+
+    dirs = os.listdir(os.getcwd())
+    for pic in dirs:
+        if pic.startswith('Photo ') and pic.endswith('.jpg'):
+            main_jpg(pic)
+        elif 'IMG_' in pic and pic.endswith('.png'):
+            main_png(pic)
+
+    print('-'*10, 'Finished', '-'*10)
