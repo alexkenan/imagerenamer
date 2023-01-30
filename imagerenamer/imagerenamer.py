d4950456cd7f3d6526da004ae679448f575e156b
Alex Kenan <alexkenan@users.noreply.github.com>
2023-01-30 11:21:29 -0500

    Update imagerenamer.py
    Updated file path since Dropbox update changed the location of the Dropbox folder

--- imagerenamer/imagerenamer.py
+++ imagerenamer/imagerenamer.py
@@ -1,6 +1,6 @@
-#!/usr/bin/env python3.5
+#!/usr/bin/env python3
 #####################################
-#    LAST UPDATED     12 MAR 2017   #
+#    LAST UPDATED     30 JAN 2023   #
 #####################################
 """
 Renames photos to Year-Month-Day Hour.Minute.Second format
@@ -8,6 +8,7 @@ Renames photos to Year-Month-Day Hour.Minute.Second format
 import re
 import os
 import datetime
+import shutil
 from PIL import Image
 from PIL.ExifTags import TAGS
 
@@ -18,21 +19,24 @@ def main_png(file: str) -> None:
     :param file: Path to a photo to be renamed
     :return: None
     """
-    img = Image.open(file)
-    print('Converting {}'.format(file))
-    creation_date = re.compile(
-        r'<photoshop:DateCreated>(\d)+-(\d)+-(\d)+T(\d)+:(\d)+:(\d)+</photoshop:DateCreated>')
-    before_time = 'blah'
-    for tag, value in img.info.items():
-        match = creation_date.search(str(value))
-        if match:
-            before_time = match.group()
+    try:
+        img = Image.open(file)
+        print('Converting {}'.format(file))
+        creation_date = re.compile(
+            r'<photoshop:DateCreated>(\d)+-(\d)+-(\d)+T(\d)+:(\d)+:(\d)+</photoshop:DateCreated>')
+        before_time = 'blah'
+        for tag, value in img.info.items():
+            match = creation_date.search(str(value))
+            if match:
+                before_time = match.group()
 
-    no_tags = before_time.replace('<photoshop:DateCreated>', '').replace('</photoshop:DateCreated>', '')
-    name = no_tags.replace(':', '.').replace('T', ' ')
-    name += '.png'
-    img.save('/Volumes/MAC2/Photos/{}'.format(name))
-    print('Saved {}'.format(name))
+        no_tags = before_time.replace('<photoshop:DateCreated>', '').replace('</photoshop:DateCreated>', '')
+        name = no_tags.replace(':', '.').replace('T', ' ')
+        name += '.png'
+        img.save('/Volumes/MAC2/Photos/{}'.format(name))
+        print('Saved {}'.format(name))
+    except ValueError:
+        print(f'Skipped {file}')
 
 
 def main_jpg(file: str) -> None:
@@ -43,36 +47,91 @@ def main_jpg(file: str) -> None:
     :param file: Path to a photo to be renamed
     :return: None
     """
-    print('Converting {}'.format(file))
     image = Image.open(file)
     info = image._getexif()
     time = 'blah'
-    for tag, value in info.items():
-        key = TAGS.get(tag, tag)
-        if key == 'DateTime':
-            time = str(value)
-        elif 'DateTime' in key:
-            time = str(value)
     try:
-        time_dt = datetime.datetime.strptime(time, '%Y:%m:%d %H:%M:%S')
-        name = '/Volumes/MAC2/Photos/{0:%Y-%m-%d %H.%M.%S}.jpg'.format(time_dt)
-    except ValueError:
-        print('Could not rename {} with the year'.format(file))
-        filename = file.replace('Photo ', '').replace('.jpg', '')
-        time_dt = datetime.datetime.strptime(filename, '%b %d, %H %M %S %p')
-        name = '/Volumes/MAC2/Photos/Y-{0:%m-%d %H.%M.%S}.jpg'.format(time_dt)
+        try:
+            for tag, value in info.items():
+                key = TAGS.get(tag, tag)
+                if isinstance(key, str):
+                    if key == 'DateTime':
+                        time = str(value)
+                    elif 'DateTime' in key:
+                        time = str(value)
+                    else:
+                        time = str(file)
+        except AttributeError:
+            print('Could not rename {} with the year, assuming it is from this year'.format(file))
+            filename = file.replace('Photo ', '').replace('.jpg', '')
 
-    image.save(name)
-    print('Saved {}'.format(name))
+            if '(1)' in filename:
+                time_delta = 1
+                filename = filename.replace('(1)', '')
+            elif '(2)' in filename:
+                time_delta = 3
+                filename = filename.replace('(2)', '')
+            elif '(3)' in filename:
+                time_delta = 3
+                filename = filename.replace('(3)', '')
+            else:
+                time_delta = 0
+
+            time_dt = datetime.datetime.strptime(filename.strip(), '%b %d, %H %M %S %p')
+
+            if time_delta:
+                time_dt = time_dt + datetime.timedelta(seconds=time_delta)
+
+            name = '/Volumes/MAC2/Photos/{0}-{1:%m-%d %H.%M.%S}.jpg'.format(datetime.datetime.now().year, time_dt)
+        try:
+            time_dt = datetime.datetime.strptime(time, '%Y:%m:%d %H:%M:%S')
+            name = '/Volumes/MAC2/Photos/{0:%Y-%m-%d %H.%M.%S}.jpg'.format(time_dt)
+        except ValueError:
+            try:
+                print(time)
+                time_dt = datetime.datetime.strptime(time, '%b %d, %H %M %S %p')
+                name = '/Volumes/MAC2/Photos/{0}-{1:%m-%d %H.%M.%S}.jpg'.format(datetime.datetime.now().year, time_dt)
+            except ValueError:
+                print('Could not find a year for {}, assuming it is from this year'.format(file))
+                filename = file.replace('Photo ', '').replace('.jpg', '')
+                time_dt = datetime.datetime.strptime(filename, '%b %d %Y, %H %M %S %p')
+                name = '/Volumes/MAC2/Photos/{0}-{1:%m-%d %H.%M.%S}.jpg'.format(datetime.datetime.now().year, time_dt)
+
+        image.save(name)
+        print('Saved {}'.format(name))
+    except ValueError:
+        print(f"Skipped {file}")
+
+
+def main_heic(file: str) -> None:
+    """
+    Used for HEIC images with EXIF data
+    :param file: Path to a photo to be renamed
+    :return: None
+    """
+    try:
+        clean_name = file.replace('Photo ', '').replace('.heic', '').strip()
+        try:
+            time_dt = datetime.datetime.strptime(clean_name, '%b %d, %H %M %S %p')
+        except ValueError:
+            time_dt = datetime.datetime.strptime(clean_name, '%Y-%m-%d %H.%M.%S')
+
+        name = '/Volumes/MAC2/Photos/{0}-{1:%m-%d %H.%M.%S}.jpg'.format(datetime.datetime.now().year, time_dt)
+        shutil.move(file, name)
+        print('Saved {}'.format(name))
+    except ValueError:
+        print(f'Skipped {file}')
 
 
 if __name__ == '__main__':
     print('-' * 10, 'Starting', '-' * 10)
-    print('Looking in /Users/Alex/Dropbox/')
-    os.chdir('/Users/Alex/Dropbox/')
+    os.chdir('/Users/alex/Library/CloudStorage/Dropbox')
+    print('Looking in {}'.format(os.getcwd()))
+    ok_filenames = ['JnJ COVID card.jpg', 'JnJ COVID card2.jpg']
 
     if os.path.exists('/Volumes/MAC2/Photos/'):
         dirs = os.listdir(os.getcwd())
+        dirs.sort()
         for pic in dirs:
             if pic.startswith('Photo ') and pic.endswith('.jpg'):
                 main_jpg(pic)
@@ -81,6 +140,17 @@ if __name__ == '__main__':
             elif 'IMG_' in pic and pic.endswith('.png'):
                 main_png(pic)
                 os.unlink(pic)
+                print('Deleted {}'.format(pic))
+            elif pic.endswith('.png'):
+                main_png(pic)
+                os.unlink(pic)
+                print('Deleted {}'.format(pic))
+            elif pic.endswith('.jpg') and pic not in ok_filenames:
+                main_jpg(pic)
+                # os.unlink(pic)
+                # print('Deleted {}'.format(pic))
+            elif pic.endswith('.heic'):
+                main_heic(pic)
                 print('Deleted {}'.format(pic))
 
         print('-'*10, 'Finished', '-'*10)
